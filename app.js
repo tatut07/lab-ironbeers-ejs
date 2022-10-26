a1cf02c4f98a11b1be2233db8ce8d19257b8ff57
Tatiana Toledo <tatiana@tatianas-mbp.speedport.ip>
2022-10-26 22:40:06 +0200

    iteration4

--- app.js
+++ app.js
@@ -19,13 +19,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 app.get('/beers', async (req, res) => {
-  const allTheBeers = await punkAPI.getBeers();
-  res.render('beers', { allTheBeers });
+  const beer = await punkAPI.getBeers();
+  res.render('beers', { beer });
 });
 app.get('/random-beer', async (req, res) => {
   const randomBeer = await punkAPI.getRandom();
-  console.log(randomBeer);
   res.render('random-beer', { randomBeer });
+});
+app.get('/beerId/:id', async (req, res) => {
+  const id = req.params.id;
+  const singleBeer = await punkAPI.getBeer(id);
+  res.render('beerId', { singleBeer });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
